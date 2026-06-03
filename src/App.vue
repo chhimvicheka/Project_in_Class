10d95a19538b94460cf9607525ddfd9723e19514
VICHEKA <chhim.vicheka168@gmail.com>
2026-06-03 13:55:58 +0700

    add hello world text to home page

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
+  <h1> Hello World</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
