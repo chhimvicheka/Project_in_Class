ca8d832b62a99b8b299474e1ec02b7f0e8ef2c3c
VICHEKA <chhim.vicheka168@gmail.com>
2026-06-04 08:59:36 +0700

    Save project with 3 pages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
+
+
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <h1> Hello World</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <RouterView />
+  <!-- <RouterView></RouterView> -->
+  <!-- <router-view /> -->
+  <!-- <router-view></router-view> -->
 </template>
 
 <style scoped></style>
