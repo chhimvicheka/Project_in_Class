94b88584c6b546f0ea920c75964f1cd82ba9db40
VICHEKA <chhim.vicheka168@gmail.com>
2026-06-10 13:30:20 +0700

    simulate api call from signin and signup page

--- src/components/auth/SignIn.vue
+++ src/components/auth/SignIn.vue
@@ -50,8 +50,9 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
 import { useRouter } from "vue-router";
+import { reactive } from "vue";
+import { LoadingModal, MessageModal, CloseModal } from "@/functions/swal";
 
 const router = useRouter();
 
@@ -65,10 +66,29 @@ const userError = reactive({
     password: "",
 });
 
+const defaultUser = JSON.parse(JSON.stringify(user));
+const defaultUserError = JSON.parse(JSON.stringify(userError));
+
+function resetAllState() {
+    Object.assign(user, defaultUser);
+    Object.assign(userError, defaultUserError);
+}
+
 async function signIn() {
-    // console.log("signIn");
-    userError.email="email errr";
-    userError.password="password error";
-    router.replace({ name:'Dashboard'   })
+    try {
+        LoadingModal('Signing In...');
+
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate API call
+
+        resetAllState();
+        router.replace({ name: "Dashboard" });
+        return CloseModal();
+    } catch (error) {
+        const { response } = error;
+        if (!response) {
+            return MessageModal({ icon: "error", title: "Error", text: error.message });
+        }
+        //!!! Handle validation errors from the server
+    }
 }
 </script>
