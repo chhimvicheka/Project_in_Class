7c5965bc8992f0aa111a665875f29ee48eadb0cc
VICHEKA <chhim.vicheka168@gmail.com>
2026-06-17 09:46:32 +0700

    Image Upload

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import SignIn from '@/components/auth/SignIn.vue';
 import SignUp from '@/components/auth/SignUp.vue';
+import SignOut from '@/components/auth/SignOut.vue';
+import Profile from '@/components/auth/Profile.vue';
 import Dashboard from '@/components/pages/Dashboard.vue';
 
 import Navbar from '@/components/includes/Navbar.vue';
 import Sidebar from '@/components/includes/Sidebar.vue';
 import Footer from '@/components/includes/Footer.vue';
+
 
 import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
@@ -12,11 +15,31 @@ const routes = [
         path: '/',
         name: 'SignIn',
         component: SignIn,
+        meta: { guarded: false },
     },
     {
         path: '/signup',
         name: 'SignUp',
         component: SignUp,
+        meta: { guarded: false },
+    },
+    {
+        path: '/signout',
+        name: 'SignOut',
+        component: SignOut,
+        // This route has no guarded meta because it use for both authenticated and unauthenticated users.
+        // The authentication state will be handled in the SignOut component.
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        components: {
+            navbar: Navbar,
+            sidebar: Sidebar,
+            footer: Footer,
+            default: Profile,
+        },
+        meta: { guarded: true },
     },
     {
         path: '/dashboard',
@@ -27,6 +50,7 @@ const routes = [
             footer: Footer,
             default: Dashboard,
         },
+        meta: { guarded: true },
     },
     { path: '/:pathMatch(.*)*', redirect: { name: 'SignIn' } },
 ];
